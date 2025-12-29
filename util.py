68454c5c1f09cb3f33a6e53db5f83827c80c7115
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2025-12-29 11:33:28 +0200

    Fix prompt index exception on chat prompt retrieval.

--- util.py
+++ util.py
@@ -13,8 +13,10 @@ SCORE_MODEL = SentenceTransformer(get_model_path("all-MiniLM-L6-v2"))
 
 def get_chat_prompt(chat_arg):
     content = next((item['content'] for item in chat_arg if item['role'] == 'user'), None)
-    prompt_index = content.index(PROMPT_TAG) + len(PROMPT_TAG)
-    return content[prompt_index:].strip()
+    prompt_tag_index = content.find(PROMPT_TAG)
+    if prompt_tag_index == -1:
+        return content
+    return content[prompt_tag_index + len(PROMPT_TAG):].strip()
 
 
 def get_similarity_score(prediction_arg, ground_truth_arg):

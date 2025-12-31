a9121979dd0d8c9ad63a08d68c441174e94ec0ab
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2025-12-31 17:03:50 +0200

    Refine max tokens utility script.

--- tokens.py
+++ tokens.py
@@ -1,30 +1,52 @@
 import yaml
 from transformers import AutoTokenizer
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--verbose", action="store_true")
+parser.add_argument("--model", action="store")
+parser.add_argument("--file", action="store")
+args = parser.parse_args()
 
 MODEL_DIR = os.environ.get("AI_MODEL_DIR")
-MODEL_NAME = MODEL_DIR + "/hugging-face/model/gemma-3-1b-it"
-YAML_FILE = "data/formatter-set.yml"
+DEFAULT_MODEL = "gemma-3-1b-it"
+DEFAULT_FILE = "formatter-set"
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model_name = args.model if args.model else DEFAULT_MODEL
+model_path = f"{MODEL_DIR}/hugging-face/model/{model_name}"
+file_name = args.file if args.file else DEFAULT_FILE
+file_path = f"data/{file_name}.yml"
+if args.verbose:
+    print(f"Use model {model_path}")
+    print(f"Use file {file_path}")
+
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
 tokenizer.truncation_side = "right"
 
-with open(YAML_FILE, 'r', encoding='utf-8') as f:
-    dataset = yaml.safe_load(f)
+if args.verbose:
+    print(f"Loading dataset {file_path} ...")
+with open(file_path, 'r', encoding='utf-8') as file:
+    dataset = yaml.safe_load(file)
+if args.verbose:
+    print(f"Loaded {len(dataset)} examples")
+    print()
 
 dataset_size = len(dataset)
 max_tokens = 0
 for index, datapoint in enumerate(dataset):
-    chat = datapoint['messages']
+    chat = datapoint['chat']
     chat_text = tokenizer.apply_chat_template(chat, tokenize=False)
     chat_tokens = tokenizer(chat_text)
     chat_tokens_count = len(chat_tokens['input_ids'])
-    print(f"{index + 1:>3} / {dataset_size}: {chat_tokens_count}")
+    if args.verbose:
+        print(f"{index + 1:>3} / {dataset_size}: {chat_tokens_count}")
     if max_tokens < chat_tokens_count:
         max_tokens = chat_tokens_count
 
-print()
+if args.verbose:
+    print()
 print(f"max_tokens: {max_tokens}")

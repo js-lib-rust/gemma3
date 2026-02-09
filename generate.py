ef2faa55b78c59b149781455d59cc28faa792479
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2026-02-09 09:35:39 +0200

    Add REPL when prompt not provided, to generate script.

--- generate.py
+++ generate.py
@@ -6,12 +6,13 @@ import util
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", action="store")
 parser.add_argument("--dtype", action="store", type=util.dtype, default="float32")
+parser.add_argument("--max-length", action="store", type=int, default="4000")
 parser.add_argument("--max-new-tokens", action="store", type=int, default="400")
 parser.add_argument("--do-sample", action="store_true")
-parser.add_argument("--samples-count", action="store", type=int, default="1")
+parser.add_argument("--num-beams", action="store", type=int, default="1")
 parser.add_argument("--template", action="store")
 parser.add_argument("--verbose", action="store_true")
-parser.add_argument("prompt", nargs="+", default=[])
+parser.add_argument("prompt", nargs="*", default=[])
 args = parser.parse_args()
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -22,9 +23,10 @@ if args.verbose:
     print(f"Use device {device}")
     print(f"Use model path {model_path}")
     print(f"Use dtype {args.dtype}")
+    print(f"Use max length {args.max_length}")
     print(f"Use max new tokens {args.max_new_tokens}")
     print(f"Use do sample {args.do_sample}")
-    print(f"Use samples count {args.samples_count}")
+    print(f"Use num beams {args.num_beams}")
     print(f"Use template {args.template}")
     print()
 
@@ -36,25 +38,43 @@ if args.verbose:
 model = AutoModelForCausalLM.from_pretrained(model_path, dtype=args.dtype)
 model.to(device)
 model.eval()
+
 if args.verbose:
     print(f"Model loaded on {device}")
-
-if args.template:
-    prompt = util.template(args.template, {'prompt': prompt})
-inputs = tokenizer(prompt, return_tensors="pt", truncation=True, max_length=4000)
-inputs = {k: v.to(device) for k, v in inputs.items()}
-
-if args.verbose:
     print("Generating response...")
     print()
 streamer = TextStreamer(tokenizer)
 config = {
     'max_new_tokens': args.max_new_tokens,
     'do_sample': args.do_sample,
+    'num_beams': args.num_beams,
     'pad_token_id': tokenizer.eos_token_id,
     'streamer': streamer
 }
-for _ in range(args.samples_count):
+
+
+def generate(prompt_arg):
+    if args.template:
+        prompt_arg = util.template(args.template, {'prompt': prompt_arg})
+    inputs = tokenizer(prompt_arg, return_tensors="pt", truncation=True, max_length=args.max_length)
+    inputs = {k: v.to(device) for k, v in inputs.items()}
     with torch.no_grad():
         model.generate(**inputs, **config)
+
+
+if prompt:
+    generate(prompt)
+
+else:
+    print(f"Generation REPL on model {model_path}")
+    print()
+
+    while True:
+        line = input("- ")
+        if line.lower() == "exit":
+            print("Exiting...")
+            break
+        generate(line)
         print()
+
+print()

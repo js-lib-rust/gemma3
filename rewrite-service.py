3c24d0653071b8a22be2f23d123261736b50d3df
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2026-03-07 11:55:31 +0200

    Add support for multiple sequences generation on rewrite service.

--- rewrite-service.py
+++ rewrite-service.py
@@ -26,40 +26,51 @@ async def handle_slm_request(request):
         print(f"chat_text: {chat_text}")
         inputs = tokenizer([chat_text], return_tensors="pt").to(model.device)
 
+        max_new_tokens = int(json_request.get('max_new_tokens', "400"))
+        do_sample = bool(json_request.get('do_sample'))
+        num_beams = int(json_request.get('num_beams', "1"))
         config = {
-            'max_new_tokens': 400,
-            'do_sample': False,
-            'num_beams': 1,
+            'max_new_tokens': max_new_tokens,
+            'do_sample': do_sample,
+            'num_beams': num_beams,
             'pad_token_id': tokenizer.eos_token_id,
             'return_dict_in_generate': True,
             'output_scores': True
         }
+        if num_beams > 1:
+            config['num_return_sequences'] = num_beams
         print(f"config: {config}")
+
         outputs = model.generate(**inputs, **config)
-        generated_ids = outputs.sequences[0][inputs["input_ids"].shape[1]:]
-
+        generated_ids = outputs.sequences
         transition_scores = model.compute_transition_scores(outputs.sequences, outputs.scores, normalize_logits=True)
-        token_probs = transition_scores[0].exp().cpu().numpy()
-        avg_confidence = float(token_probs.mean())
-        min_confidence = float(token_probs.min())
 
-        output_text = tokenizer.decode(generated_ids, skip_special_tokens=True)
-        print(f"output_text: {output_text}")
-        print(f"confidence: {avg_confidence:.4f} (min: {min_confidence:.4f})")
+        response = []
+        for sequence_index in range(len(generated_ids)):
+            sequence_generated_ids = generated_ids[sequence_index][inputs["input_ids"].shape[1]:]
 
-        # agent1:prompt1\nagent2:prompt2\nagent3:prompt3
-        actions = []
-        for line in output_text.split("\n"):
-            line_parts = line.split(":", 1)
-            actions.append({"agent": f"{line_parts[0]}", "prompt": f"{line_parts[1]}"})
+            sequence_transition_scores = transition_scores[sequence_index]
+            token_probs = sequence_transition_scores.exp().cpu().numpy()
+            avg_confidence = float(token_probs.mean())
+            min_confidence = float(token_probs.min())
 
-        response = {
-            "actions": actions,
-            "confidence": avg_confidence,
-            "confidence_min": min_confidence
-        }
+            output_text = tokenizer.decode(sequence_generated_ids, skip_special_tokens=True)
+            print(f"output_text: {output_text}")
+            print(f"confidence: {avg_confidence:.4f} (min: {min_confidence:.4f})")
+
+            # agent1:prompt1\nagent2:prompt2\nagent3:prompt3
+            actions = []
+            for line in output_text.split("\n"):
+                line_parts = line.split(":", 1)
+                actions.append({"agent": f"{line_parts[0]}", "prompt": f"{line_parts[1]}"})
+
+            response.append({
+                "actions": actions,
+                "confidence": avg_confidence,
+                "confidence_min": min_confidence
+            })
+
         print(f"response: {response}")
-
         print(f"Request processing time: {time.time() - request_start_time}")
         return web.json_response(response, status=200)
 

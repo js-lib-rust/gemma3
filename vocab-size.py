0bf43c65fb48ac7c03fd2fe3dbfbb1e0f2730fdf
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2026-05-02 06:26:08 +0300

    Add support for stock models to vocabulary size script.

--- vocab-size.py
+++ vocab-size.py
@@ -1,18 +1,21 @@
 import argparse
 import json
+import util
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", action="store", type=str, required=True)
 args = parser.parse_args()
 
-model = args.model
-tokenizer_path = f"{model}/tokenizer.json" 
+model_path = util.get_model_path(args.model) if args.model.startswith('/') else args.model
+print(f"Use model path {model_path}")
 
-with open(tokenizer_path, 'r') as f:
+tokenizer_path = f"{model_path}/tokenizer.json"
+
+with open(tokenizer_path, 'r', encoding='UTF-8') as f:
     vocab_json = json.load(f)
 
 vocab = vocab_json['model']['vocab']
 max_id = max(vocab.values())
 vocab_size = max_id + 1
 
-print(f"Vocabulary size for model {model} is: {vocab_size}")
+print(f"Vocabulary size for model {model_path} is: {vocab_size}")

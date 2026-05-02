3532346d0a9cf7c803b82379b4e3b2cef2b51632
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2026-05-02 06:19:28 +0300

    Add 'model' command argument to vocabulary size script.

--- vocab-size.py
+++ vocab-size.py
@@ -1,14 +1,18 @@
+import argparse
 import json
 
-model = "router-270m"
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", action="store", type=str, required=True)
+args = parser.parse_args()
+
+model = args.model
 tokenizer_path = f"{model}/tokenizer.json" 
 
 with open(tokenizer_path, 'r') as f:
-    data = json.load(f)
+    vocab_json = json.load(f)
 
-vocab = data['model']['vocab']
+vocab = vocab_json['model']['vocab']
 max_id = max(vocab.values())
 vocab_size = max_id + 1
 
-print(f"Vocabular size for model {model} is: {vocab_size}")
-
+print(f"Vocabulary size for model {model} is: {vocab_size}")

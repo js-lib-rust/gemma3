4067e188e89e2eca58dec57649e3634d6b904076
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2026-07-05 17:21:21 +0300

    Refine datasets for router.

--- schema/__init__.py
+++ schema/__init__.py
@@ -2,12 +2,12 @@ import importlib
 from transformers.utils import get_json_schema
 
 from .hera import *
-from .home_automation import *
+from schema.xp.home_automation import *
 
 __version__ = (1, 0, 0)
 
 
 def get(module_name: str):
     module = importlib.import_module('.' + module_name, 'schema')
-    functions = [attr for attr in dir(module) if not attr.startswith('__')]
+    functions = [attr for attr in dir(module) if attr.startswith(module_name)]
     return [get_json_schema(getattr(module, function)) for function in functions]
